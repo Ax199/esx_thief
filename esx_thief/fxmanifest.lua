19d62f4971507e851864627fdac554b0ac16b181
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-14 14:53:46 -0300

    Add files via upload

---
+++ esx_thief/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'ESX THIEF'
+
+version '1.0.3'
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/es.lua',
+  'locales/fr.lua',
+  'locales/sv.lua',
+  'locales/pl.lua',
+  'config.lua',
+  'client/main.lua',
+  'handsup.lua'
+}
+
+server_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/es.lua',
+  'locales/fr.lua',
+  'locales/sv.lua',
+  'locales/pl.lua',
+  'config.lua',
+  'server/main.lua'
+}
